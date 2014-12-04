aff095d2baf910405b6f40e4ab924ac473eaf49f
Alex Van Camp <email@alexvan.camp>
2014-12-04 14:06:32 -0600

    make pretest create a cfg dir

--- test/setup/pretest.js
+++ test/setup/pretest.js
@@ -8,6 +8,11 @@ var BUNDLE_DIR = path.resolve(process.cwd(), 'bundles', BUNDLE_NAME);
 var CFG_PATH = path.resolve(process.cwd(), 'cfg', BUNDLE_NAME + '.json');
 var TEST_BUNDLE_SRC_PATH = path.resolve(process.cwd(), 'test/setup/', BUNDLE_NAME);
 
+// Create the cfg dir if it does not exist
+if (!fs.existsSync('cfg/')) {
+    fs.mkdirSync('cfg');
+}
+
 wrench.copyDirSyncRecursive(TEST_BUNDLE_SRC_PATH, BUNDLE_DIR, {
     forceDelete: true
 });
