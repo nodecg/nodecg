3669a513c95a6789fcc53a3d440ac659f9dc7604
Alex Van Camp <email@alexvan.camp>
2014-12-04 14:08:28 -0600

    using what we like to call "the right way"

--- test/setup/pretest.js
+++ test/setup/pretest.js
@@ -5,15 +5,16 @@ var path = require('path');
 
 var BUNDLE_NAME = 'test-bundle';
 var BUNDLE_DIR = path.resolve(process.cwd(), 'bundles', BUNDLE_NAME);
-var CFG_PATH = path.resolve(process.cwd(), 'cfg', BUNDLE_NAME + '.json');
+var CFG_DIR = path.resolve(process.cwd(), 'cfg');
+var BUNDLE_CFG_PATH = path.resolve(CFG_DIR, BUNDLE_NAME + '.json');
 var TEST_BUNDLE_SRC_PATH = path.resolve(process.cwd(), 'test/setup/', BUNDLE_NAME);
 
 // Create the cfg dir if it does not exist
-if (!fs.existsSync('cfg/')) {
-    fs.mkdirSync('cfg');
+if (!fs.existsSync(CFG_DIR)) {
+    fs.mkdirSync(CFG_DIR);
 }
 
 wrench.copyDirSyncRecursive(TEST_BUNDLE_SRC_PATH, BUNDLE_DIR, {
     forceDelete: true
 });
-fs.writeFileSync(CFG_PATH, JSON.stringify({ test: "data" }));
+fs.writeFileSync(BUNDLE_CFG_PATH, JSON.stringify({ test: "data" }));
