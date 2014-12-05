4e9e2bfc13763af0c64177ae4bd0f120fc9ba063
Alex Van Camp <email@alexvan.camp>
2014-12-04 20:35:58 -0600

    better tests for jade panels, add tests for html panels

--- test/dashboard.js
+++ test/dashboard.js
@@ -2,6 +2,7 @@
 
 // Modules used to run tests
 var Browser = require('zombie');
+var config = require(process.cwd() + '/lib/config').config;
 
 var C = require('./setup/test-constants');
 
@@ -20,9 +21,25 @@ before(function(done) {
         .then(done, done);
 });
 
-describe("panels", function() {
-    it("if Jade, have access to bundleConfig", function() {
-        dashboardBrowser.assert.text('#test-bundle .js-test', 'the_test_string');
+describe("dashboard", function() {
+    describe("html panels", function() {
+        it("show up on the dashboard", function() {
+            dashboardBrowser.assert.element('#test-bundle_html');
+        });
+    });
+
+    describe("jade panels", function() {
+        it("show up on the dashboard", function() {
+            dashboardBrowser.assert.element('#test-bundle_jade');
+        });
+
+        it("have access to bundleConfig", function() {
+            dashboardBrowser.assert.text('#test-bundle_jade .js-bundleConfig', 'the_test_string');
+        });
+
+        it("have access to ncgConfig", function() {
+            dashboardBrowser.assert.text('#test-bundle_jade .js-ncgConfig', config.host);
+        });
     });
 });
 
