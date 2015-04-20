7aac9c02dbbfa3604890858991f83b7b08287e80
thesupremecommander <thesupremecmdr@gmail.com>
2015-04-19 22:51:44 -0400

    Rewrite replicant tests.

--- test/clientApi.js
+++ test/clientApi.js
@@ -68,18 +68,15 @@ describe('client api', function() {
             e.browsers.dashboard
                 .executeAsync(function(done) {
                     try {
-                        e.apis.dashboard.config.host = 'the_test_failed';
+                        window.dashboardApi.config.host = 'the_test_failed';
                     }
                     catch (e) {
-                        done(e);
+                        done(e instanceof TypeError);
                     }
-                    window.dashboardApi.listenFor('dashboardToServer', done);
-                }, function(err, e) {
-                    expect(function() {
-                        throw e;
-                    }).to.throw(TypeError);
-                })
-                .call(done);
+                }, function(err, isTypeError) {
+                    expect(isTypeError).to.be.true;
+                    done();
+                });
         });
     });
 
@@ -98,114 +95,145 @@ describe('client api', function() {
     describe('replicants', function() {
         it('only apply defaultValue when first declared', function(done) {
             e.apis.extension.Replicant('clientTest', { defaultValue: 'foo', persistent: false });
-            var rep = e.apis.dashboard.Replicant('clientTest', { defaultValue: 'bar' });
+            e.browsers.dashboard
+                .executeAsync(function(done) {
+                    var rep = window.dashboardApi.Replicant('clientTest', { defaultValue: 'bar' });
 
-            // Give Zombie a chance to process socket.io events
-            e.browsers.dashboard.wait({duration: 10}, function() {});
-
-            setTimeout(function() {
-                expect(rep.value).to.equal('foo');
-                done();
-            }, 50);
+                    rep.on('change', function() {
+                        done(rep.value);
+                    });
+                }, function(err, replicantValue) {
+                    expect(replicantValue).to.equal('foo');
+                    done();
+                });
         });
 
         it('can be read once without subscription, via readReplicant', function(done) {
-            e.apis.dashboard.readReplicant('clientTest', function(value) {
-                expect(value).to.equal('foo');
-                done();
-            });
+            e.browsers.dashboard
+                .executeAsync(function(done) {
+                    window.dashboardApi.readReplicant('clientTest', function(value) {
+                        done(value);
+                    });
+                }, function(err, replicantValue) {
+                    expect(replicantValue).to.equal('foo');
+                    done();
+                });
         });
 
-        it('throws an error when no name is given to a synced variable', function () {
-            expect(function() {
-                e.apis.dashboard.Replicant();
-            }).to.throw(/Must supply a name when instantiating a Replicant/);
+        it('throws an error when no name is given to a synced variable', function(done) {
+            e.browsers.dashboard
+                .executeAsync(function(done) {
+                    try {
+                        window.dashboardApi.Replicant();
+                    }
+                    catch (e) {
+                        done(e.message);
+                    }
+                }, function(err, errorMessage) {
+                    expect(errorMessage).to.equal('Must supply a name when instantiating a Replicant');
+                    done();
+                });
         });
 
         it('can be assigned via the ".value" property', function (done) {
-            var rep = e.apis.dashboard.Replicant('clientAssignmentTest', { persistent: false });
-            rep.on('assignmentAccepted', function(data) {
-                expect(data.value).to.equal('assignmentOK');
-                expect(data.revision).to.equal(1);
-                done();
-            });
-            rep.value = 'assignmentOK';
-
-            // Give Zombie a chance to process socket.io events.
-            // I don't know why I have to micromanage the event loop like this.
-            // This is dumb.
-            // I am mad.
-            e.browsers.dashboard.wait({duration: 10}, function() {
-                e.browsers.dashboard.wait({duration: 10}, function() {});
-            });
+            e.browsers.dashboard
+                .executeAsync(function(done) {
+                    var rep = window.dashboardApi.Replicant('clientAssignmentTest', { persistent: false });
+                    rep.on('assignmentAccepted', function(data) {
+                        done(data);
+                    });
+                    rep.value = 'assignmentOK';
+                }, function(err, data) {
+                    expect(data.value).to.equal('assignmentOK');
+                    expect(data.revision).to.equal(1);
+                    done();
+                });
         });
 
         // Skipping this test for now because for some reason changes to the value object
         // aren't triggering the Nested.observe callback.
         it.skip('reacts to changes in nested properties of objects', function(done) {
-            var rep = e.apis.dashboard.Replicant('clientObjTest', {
-                persistent: false,
-                defaultValue: {
-                    a: {
-                        b: {
-                            c: 'c'
+            e.browsers.dashboard
+                .executeAsync(function(done) {
+                    var rep = window.dashboardApi.Replicant('clientObjTest', {
+                        persistent: false,
+                        defaultValue: {
+                            a: {
+                                b: {
+                                    c: 'c'
+                                }
+                            }
                         }
-                    }
-                }
-            });
+                    });
 
-            rep.on('change', function(oldVal, newVal, changes) {
-                expect(oldVal).to.deep.equal({a: {b: {c: 'c'}}});
-                expect(newVal).to.deep.equal({a: {b: {c: 'nestedChangeOK'}}});
-                expect(changes).to.have.length(1);
-                expect(changes[0].type).to.equal('update');
-                expect(changes[0].path).to.equal('a.b.c');
-                expect(changes[0].oldValue).to.equal('c');
-                expect(changes[0].newValue).to.equal('nestedChangeOK');
-                done();
-            });
-
-            // This is FUCKING stupid.
-            e.browsers.dashboard.wait({duration: 10}, function() {
-                rep.value.a.b.c = 'nestedChangeOK';
-
-                e.browsers.dashboard.wait({duration: 10}, function() {
-                    e.browsers.dashboard.wait({duration: 10}, function() {});
+                    rep.on('change', function(oldVal, newVal, changes) {
+                        done({
+                            oldVal: oldVal,
+                            newVal: newVal,
+                            changes: changes
+                        })
+                    });
+                }, function(err, data) {
+                    expect(data.oldVal).to.deep.equal({a: {b: {c: 'c'}}});
+                    expect(data.newVal).to.deep.equal({a: {b: {c: 'nestedChangeOK'}}});
+                    expect(data.changes).to.have.length(1);
+                    expect(data.changes[0].type).to.equal('update');
+                    expect(data.changes[0].path).to.equal('a.b.c');
+                    expect(data.changes[0].oldValue).to.equal('c');
+                    expect(data.changes[0].newValue).to.equal('nestedChangeOK');
+                    done();
                 });
-            });
         });
 
         it('load persisted values when they exist', function(done) {
             // Make sure the persisted value exists
             fs.writeFileSync('./db/replicants/test-bundle.clientPersistence', 'it work good!');
 
-            var rep = e.apis.dashboard.Replicant('clientPersistence');
-            e.browsers.dashboard.wait({duration: 10}, function() {
-                expect(rep.value).to.equal('it work good!');
-                done();
-            });
+            e.browsers.dashboard
+                .executeAsync(function(done) {
+                    var rep = window.dashboardApi.Replicant('clientPersistence');
+
+                    rep.on('change', function() {
+                        done(rep.value);
+                    });
+                }, function(err, replicantValue) {
+                    expect(replicantValue).to.equal('it work good!');
+                    done();
+                });
         });
 
         it('persist assignment to disk', function(done) {
-            var rep = e.apis.dashboard.Replicant('clientPersistence');
-            rep.value = { nested: 'hey we assigned!' };
-            e.browsers.dashboard.wait({duration: 10}, function() {
-                var persistedValue = fs.readFileSync('./db/replicants/test-bundle.clientPersistence', 'utf-8');
-                expect(persistedValue).to.equal('{"nested":"hey we assigned!"}');
-                done();
-            });
+            e.browsers.dashboard
+                .executeAsync(function(done) {
+                    var rep = window.dashboardApi.Replicant('clientPersistence');
+                    rep.value = { nested: 'hey we assigned!' };
+
+                    rep.on('change', function() {
+                        done();
+                    });
+                }, function(err) {
+                    var persistedValue = fs.readFileSync('./db/replicants/test-bundle.clientPersistence', 'utf-8');
+                    expect(persistedValue).to.equal('{"nested":"hey we assigned!"}');
+                    done();
+                });
         });
 
         // Skipping this test for now because for some reason changes to the value object
         // aren't triggering the Nested.observe callback.
         it.skip('persist changes to disk', function(done) {
-            var rep = e.apis.dashboard.Replicant('clientPersistence');
-            rep.value.nested = 'hey we changed!';
-            e.browsers.dashboard.wait({duration: 10}, function() {
-                var persistedValue = fs.readFileSync('./db/replicants/test-bundle.clientPersistence', 'utf-8');
-                expect(persistedValue).to.equal('{"nested":"hey we changed!"}');
-                done();
-            });
+            e.browsers.dashboard
+                .executeAsync(function(done) {
+                    var rep = window.dashboardApi.Replicant('clientPersistence');
+                    rep.value.nested = 'hey we changed!';
+
+                    rep.on('change', function() {
+                        done();
+                    });
+                }, function(err) {
+                    var persistedValue = fs.readFileSync('./db/replicants/test-bundle.clientPersistence', 'utf-8');
+                    expect(persistedValue).to.equal('{"nested":"hey we changed!"}');
+                    done();
+                });
         });
 
         it('don\'t persist when "persistent" is set to "false"', function(done) {
@@ -214,17 +242,23 @@ describe('client api', function() {
                 fs.unlinkSync('./db/replicants/test-bundle.clientTransience');
             } catch(e) {}
 
-            e.apis.dashboard.Replicant('clientTransience', { defaultValue: 'o no', persistent: false });
-            e.browsers.dashboard.wait({duration: 10}, function() {
-                var exists = fs.existsSync('./db/replicants/test-bundle.clientTransience');
-                expect(exists).to.be.false;
-                done();
-            });
+            e.browsers.dashboard
+                .executeAsync(function(done) {
+                    window.dashboardApi.Replicant('clientTransience', { defaultValue: 'o no', persistent: false });
+
+                    rep.on('declared', function() {
+                        done();
+                    });
+                }, function(err) {
+                    var exists = fs.existsSync('./db/replicants/test-bundle.clientTransience');
+                    expect(exists).to.be.false;
+                    done();
+                });
         });
 
         it.skip('redeclare after reconnecting to Socket.IO', function(done) {
             this.timeout(30000);
-            var rep = e.apis.dashboard.Replicant('clientRedeclare', { defaultValue: 'foo', persistent: false });
+            var rep = window.dashboardApi.Replicant('clientRedeclare', { defaultValue: 'foo', persistent: false });
             e.browsers.dashboard.wait({duration: 10}, function() {});
             rep.once('declared', function() {
                 e.server.once('stopped', function() {
