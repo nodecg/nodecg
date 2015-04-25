d1f62f42e43decb563e6613632cf1a6469db9964
Alex Van Camp <email@alexvan.camp>
2015-04-25 10:36:09 -0500

    [tests] Add support for running tests with a local Selenium instance

--- test/setup.js
+++ test/setup.js
@@ -22,37 +22,49 @@ before(function(done) {
         /** Extension API setup **/
         e.apis.extension = e.server.getExtensions()[C.BUNDLE_NAME];
 
-        var desiredCapabilities = {
-            name: "Travis job " + process.env.TRAVIS_JOB_NUMBER,
-            build: process.env.TRAVIS_BUILD_NUMBER,
-            tags: [process.env.TRAVIS_BRANCH, process.env.TRAVIS_COMMIT, process.env.TRAVIS_COMMIT_RANGE],
-            browserName: 'chrome',
-            version: 'beta',
-            tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
-        };
+        if (process.env.TRAVIS_OS_NAME && process.env.TRAVIS_JOB_NUMBER) {
+            console.log('Travis environment detected, running WebDriver.io with Travis capabilities');
+            var desiredCapabilities = {
+                name: 'Travis job ' + process.env.TRAVIS_JOB_NUMBER,
+                build: process.env.TRAVIS_BUILD_NUMBER,
+                tags: [process.env.TRAVIS_BRANCH, process.env.TRAVIS_COMMIT, process.env.TRAVIS_COMMIT_RANGE],
+                browserName: 'chrome',
+                version: 'beta',
+                tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
+            };
 
-        if (process.env.TRAVIS_PULL_REQUEST !== 'false') {
-            desiredCapabilities.tags.push(process.env.TRAVIS_PULL_REQUEST);
+            if (process.env.TRAVIS_PULL_REQUEST !== 'false') {
+                desiredCapabilities.tags.push(process.env.TRAVIS_PULL_REQUEST);
+            }
+
+            if (process.env.TRAVIS_TAG) {
+                desiredCapabilities.tags.push(process.env.TRAVIS_TAG);
+            }
+
+            if (process.env.TRAVIS_OS_NAME === 'linux') {
+                desiredCapabilities.platform = 'Linux';
+            }
+            else if (process.env.TRAVIS_OS_NAME === 'osx') {
+                desiredCapabilities.platform = 'OS X 10.10';
+            }
+
+            e.browser.client = webdriverio.remote({
+                desiredCapabilities: desiredCapabilities,
+                host: 'ondemand.saucelabs.com',
+                port: 80,
+                user: process.env.SAUCE_USERNAME,
+                key: process.env.SAUCE_ACCESS_KEY
+            });
+        } else {
+            console.log('Running WebDriver.io with local capabilities');
+            e.browser.client = webdriverio.remote({
+                desiredCapabilities: {
+                    browserName: 'chrome'
+                }
+            });
         }
 
-        if (process.env.TRAVIS_TAG) {
-            desiredCapabilities.tags.push(process.env.TRAVIS_TAG);
-        }
-
-        if (process.env.TRAVIS_OS_NAME === 'linux') {
-            desiredCapabilities.platform = 'Linux';
-        }
-        else if (process.env.TRAVIS_OS_NAME === 'osx') {
-            desiredCapabilities.platform = 'OS X 10.10';
-        }
-
-        e.browser.client = webdriverio.remote({
-            desiredCapabilities: desiredCapabilities,
-            host: 'ondemand.saucelabs.com',
-            port: 80,
-            user: process.env.SAUCE_USERNAME,
-            key: process.env.SAUCE_ACCESS_KEY
-        })
+        e.browser.client
             .init()
             .timeoutsAsyncScript(10000)
             .newWindow(C.DASHBOARD_URL, 'NodeCG dashboard', '')
