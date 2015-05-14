bbb8e2a0bfd60a06c13575c6b52a72b873f8d60f
thesupremecommander <thesupremecmdr@gmail.com>
2015-05-13 22:41:08 -0400

    Update dashboard tests.

--- test/dashboard.js
+++ test/dashboard.js
@@ -10,16 +10,55 @@ var e = require('./setup/test-environment');
 describe('dashboard', function() {
     this.timeout(10000);
 
-    describe('html panels', function() {
+    describe('panels', function() {
         it('show up on the dashboard', function(done) {
             e.browser.client
                 .switchTab(e.browser.tabs.dashboard)
-                .isExisting('.test-bundle.html', function(err, isExisting) {
+                .isExisting('nodecg-test-panel', function(err, isExisting) {
                     if (err) {
                         throw err;
                     }
 
                     expect(isExisting).to.be.true;
+                })
+                .call(done);
+        });
+
+        it('have access to bundleConfig', function(done) {
+            e.browser.client
+                .switchTab(e.browser.tabs.dashboard)
+                .getText('nodecg-test-panel /deep/ #bundleConfig', function(err, text) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(text).to.equal('the_test_string');
+                })
+                .call(done);
+        });
+
+        it('have access to bundleName', function(done) {
+            e.browser.client
+                .switchTab(e.browser.tabs.dashboard)
+                .getText('nodecg-test-panel /deep/ #bundleName', function(err, text) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(text).to.equal('test-bundle');
+                })
+                .call(done);
+        });
+
+        it('have access to ncgConfig', function(done) {
+            e.browser.client
+                .switchTab(e.browser.tabs.dashboard)
+                .getText('nodecg-test-panel /deep/ #ncgConfig', function(err, text) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(text).to.equal(config.host);
                 })
                 .call(done);
         });
