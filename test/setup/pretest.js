42a81320b287ddf1162b54b3523f16988d5da042
Alex Van Camp <email@alexvan.camp>
2015-08-27 12:45:52 -0500

    [tests] Prevent error when test-bundle replicant dir doesn't exist

--- test/setup/pretest.js
+++ test/setup/pretest.js
@@ -15,4 +15,8 @@ wrench.copyDirSyncRecursive(C.TEST_BUNDLE_SRC_PATH, C.BUNDLE_DIR, { forceDelete:
 fs.writeFileSync(C.BUNDLE_CFG_PATH, JSON.stringify({ test: 'the_test_string' }));
 
 // Delete any existing persisted replicants for the test bundle
-wrench.rmdirSyncRecursive(path.resolve(__dirname, '../../db/replicants/test-bundle'));
+var replicantDir = path.resolve(__dirname, '../../db/replicants/test-bundle');
+if (fs.existsSync(replicantDir)) {
+    wrench.rmdirSyncRecursive(replicantDir);
+}
+
