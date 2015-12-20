9676ea7005d83864541ed2f8fa7ee5cb17f81fbd
Alex Van Camp <email@alexvan.camp>
2015-12-20 14:51:43 -0600

    Use new @nodecg/logger and @nodecg/bundle-manager libs

--- test/bower_deps.js
+++ test/bower_deps.js
@@ -3,23 +3,9 @@
 var chai = require('chai');
 var expect = chai.expect;
 var request = require('request');
-var path = require('path');
-var fs = require('fs');
 var C = require('./setup/test-constants');
 
-describe('per-bundle npm packages', function() {
-    it('should get installed', function () {
-        var dir = path.join(C.BUNDLE_DIR, 'node_modules/commander');
-        expect(fs.existsSync(dir)).to.be.true();
-    });
-});
-
-describe('per-bundle bower packages', function() {
-    it('should get installed', function () {
-        var dir = path.join(C.BUNDLE_DIR, 'bower_components/webcomponentsjs');
-        expect(fs.existsSync(dir)).to.be.true();
-    });
-
+describe('per-bundle bower dependencies', function() {
     it('should be accessible via /panel', function(done) {
         request(C.PANEL_COMPONENTS_URL + '/webcomponentsjs/webcomponents.js', function (error, response) {
             expect(error).to.be.null();
