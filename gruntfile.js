7d6b6442c03019e0a55c6133c7576868417dfec1
Alex Van Camp <email@alexvan.camp>
2016-03-30 11:26:26 -0500

    refactor(util): refactor util lib

--- gruntfile.js
+++ gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
 					ignore: [
 						'./lib/server/index.js',
 						'./lib/replicator.js',
-						'./lib/util.js'
+						'./lib/util/index.js'
 					]
 				}
 			},
