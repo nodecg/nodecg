ff28d0f12c6ea422cc70ccf479542147353a2ba3
Alex Van Camp <email@alexvan.camp>
2016-03-30 13:17:12 -0500

    chore(deps): remove deprecated wrench dep, replace with fs.extra

--- test/setup/pretest.js
+++ test/setup/pretest.js
@@ -2,8 +2,7 @@
 
 // Set up test bundle
 var path = require('path');
-var fs = require('fs');
-var wrench = require('wrench');
+var fs = require('fs.extra');
 var C = require('./test-constants');
 
 // Create the cfg dir if it does not exist
@@ -11,12 +10,24 @@ if (!fs.existsSync(C.CFG_DIR)) {
 	fs.mkdirSync(C.CFG_DIR);
 }
 
-wrench.copyDirSyncRecursive(C.TEST_BUNDLE_SRC_PATH, C.BUNDLE_DIR, {forceDelete: true});
-fs.writeFileSync(C.BUNDLE_CFG_PATH, JSON.stringify({test: 'the_test_string'}));
+fs.rmrf(C.BUNDLE_DIR, function (err) {
+	if (err) {
+		throw err;
+	}
+
+	fs.copyRecursive(C.TEST_BUNDLE_SRC_PATH, C.BUNDLE_DIR, function (err) {
+		if (err) {
+			throw err;
+		}
+	});
+});
+
+fs.writeFile(C.BUNDLE_CFG_PATH, JSON.stringify({test: 'the_test_string'}));
 
 // Delete any existing persisted replicants for the test bundle
 var replicantDir = path.resolve(__dirname, '../../db/replicants/test-bundle');
-if (fs.existsSync(replicantDir)) {
-	wrench.rmdirSyncRecursive(replicantDir);
-}
-
+fs.rmrf(replicantDir, function (err) {
+	if (err) {
+		throw err;
+	}
+});
