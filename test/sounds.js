3d80dd1daf9bfcf8a212bc666918710375887394
Alex Van Camp <email@alexvan.camp>
2016-04-08 12:17:50 -0500

    test(sounds): test that newly added assets are selectable in the cue assignment dropdowns

--- test/sounds.js
+++ test/sounds.js
@@ -2,10 +2,52 @@
 /* global createjs */
 'use strict';
 
-const e = require('./setup/test-environment');
+const fs = require('fs.extra');
 const assert = require('chai').assert;
+const e = require('./setup/test-environment');
+const C = require('./setup/test-constants');
 
-describe('sounds', function () {
+describe('sounds - mixer page', function () {
+	this.timeout(10000);
+
+	describe('assignable cues', () => {
+		it('should list new sound Assets as they are uploaded', done => {
+			/*
+			 1. Switch to Dashboard tab
+			 2. Open Mixer by using hashbang URL
+			 3. Add a sound file directly to nodecg/assets/test-bundle/sounds
+			 4. Check the list of options in the dropdown select for all assignable cues
+			 */
+			e.browser.client
+				.switchTab(e.browser.tabs.dashboard)
+				.url(C.MIXER_URL)
+				.then(() => new Promise((resolve, reject) => {
+					fs.copy('test/fixtures/assets/test-bundle/sounds/success.ogg',
+						'assets/test-bundle/sounds/success.ogg', {replace: true}, err => {
+							if (err) {
+								reject(err);
+								return;
+							}
+
+							resolve();
+						});
+				}))
+				.selectorExecuteAsync('#cues > ncg-sound-cue:nth-child(1) > #select', (elements, done) => {
+					const interval = setInterval(() => {
+						const options = elements[0].options;
+						if (options.length === 2 && options[1].value === 'success.ogg') {
+							clearInterval(interval);
+							done();
+						}
+					}, 50);
+				})
+				.then(() => done())
+				.catch(err => done(err));
+		});
+	});
+});
+
+describe('sounds - client api', function () {
 	this.timeout(10000);
 
 	before(done => {
@@ -14,60 +56,58 @@ describe('sounds', function () {
 			.then(() => done());
 	});
 
-	describe('client api', () => {
-		it('should emit "ncgSoundsReady" once all the sounds have loaded', done => {
-			e.browser.client
-				.executeAsync(done => {
-					if (window.graphicApi.soundsReady) {
-						done();
-					} else {
-						window.addEventListener('ncgSoundsReady', () => done());
-					}
-				})
-				.then(() => done())
-				.catch(done);
-		});
-
-		it('#playSound should return a playing AbstractAudioInstance', done => {
-			e.browser.client
-				.execute(() => {
-					return window.graphicApi.playSound('default-file').playState;
-				})
-				.then(ret => {
-					assert.equal(ret.value, 'playSucceeded');
+	it('should emit "ncgSoundsReady" once all the sounds have loaded', done => {
+		e.browser.client
+			.executeAsync(done => {
+				if (window.graphicApi.soundsReady) {
 					done();
-				})
-				.catch(done);
-		});
+				} else {
+					window.addEventListener('ncgSoundsReady', () => done());
+				}
+			})
+			.then(() => done())
+			.catch(done);
+	});
 
-		it('#stopSound should stop all instances of a cue', done => {
-			e.browser.client
-				.execute(() => {
-					window.graphicApi.playSound('default-file');
-					window.graphicApi.playSound('default-file');
-					window.graphicApi.stopSound('default-file');
-					return createjs.Sound._instances.length;
-				})
-				.then(ret => {
-					assert.equal(ret.value, 0);
-					done();
-				})
-				.catch(done);
-		});
+	it('#playSound should return a playing AbstractAudioInstance', done => {
+		e.browser.client
+			.execute(() => {
+				return window.graphicApi.playSound('default-file').playState;
+			})
+			.then(ret => {
+				assert.equal(ret.value, 'playSucceeded');
+				done();
+			})
+			.catch(done);
+	});
 
-		it('#stopAllSounds should stop all instances', done => {
-			e.browser.client
-				.execute(() => {
-					window.graphicApi.playSound('default-file');
-					window.graphicApi.playSound('default-file');
-					window.graphicApi.stopAllSounds();
-					return createjs.Sound._instances.length;
-				})
-				.then(ret => {
-					assert.equal(ret.value, 0);
-					done();
-				})
-				.catch(done);
-		});
+	it('#stopSound should stop all instances of a cue', done => {
+		e.browser.client
+			.execute(() => {
+				window.graphicApi.playSound('default-file');
+				window.graphicApi.playSound('default-file');
+				window.graphicApi.stopSound('default-file');
+				return createjs.Sound._instances.length;
+			})
+			.then(ret => {
+				assert.equal(ret.value, 0);
+				done();
+			})
+			.catch(done);
+	});
+
+	it('#stopAllSounds should stop all instances', done => {
+		e.browser.client
+			.execute(() => {
+				window.graphicApi.playSound('default-file');
+				window.graphicApi.playSound('default-file');
+				window.graphicApi.stopAllSounds();
+				return createjs.Sound._instances.length;
+			})
+			.then(ret => {
+				assert.equal(ret.value, 0);
+				done();
+			})
+			.catch(done);
 	});
 });
