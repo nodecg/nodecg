bebcd7f5ad57cc56f974f836c83ecc4d5e5d67cd
Alex Van Camp <email@alexvan.camp>
2016-04-08 11:24:30 -0500

    refactor(tests): organize test fixtures into "fixtures" folder

--- test/setup/test-constants.js
+++ test/setup/test-constants.js
@@ -1,33 +1,24 @@
 'use strict';
 
 const path = require('path');
-const util = require('util');
 
 const rootDir = path.resolve(__dirname, '../..');
 const config = require(path.join(rootDir, '/lib/config')).config;
-
 const bundleName = 'test-bundle';
-const panelName = 'test';
-const testBundleSrcPath = path.resolve(rootDir, 'test/setup/', bundleName);
-const bundleDir = path.resolve(rootDir, 'bundles', bundleName);
-const cfgDir = path.resolve(rootDir, 'cfg');
-const bundleCfgPath = path.resolve(cfgDir, `${bundleName}.json`);
-const dashboardUrl = util.format('http://%s/', config.baseURL);
-const testPanelUrl = `${dashboardUrl}panel/${bundleName}/${panelName}`;
-const panelComponentsUrl = `${dashboardUrl}panel/${bundleName}/components`;
-const graphicUrl = `${dashboardUrl}graphics/${bundleName}`;
-const singleInstanceUrl = `${dashboardUrl}graphics/${bundleName}/single_instance.html`;
+const cfgDir = path.join(rootDir, 'cfg');
+const dashboardUrl = `http://${config.baseURL}`;
 
 module.exports = {
 	BUNDLE_NAME: bundleName,
-	TEST_BUNDLE_SRC_PATH: testBundleSrcPath,
-	BUNDLE_DIR: bundleDir,
+	TEST_BUNDLE_SRC_PATH: path.join(rootDir, 'test/fixtures/bundles', bundleName),
+	BUNDLE_DIR: path.join(rootDir, 'bundles', bundleName),
 	CFG_DIR: cfgDir,
-	BUNDLE_CFG_PATH: bundleCfgPath,
+	BUNDLE_CFG_PATH: path.join(cfgDir, `${bundleName}.json`),
 	DASHBOARD_URL: dashboardUrl,
-	TEST_PANEL_URL: testPanelUrl,
-	PANEL_COMPONENTS_URL: panelComponentsUrl,
-	GRAPHIC_URL: graphicUrl,
-	SINGLE_INSTANCE_URL: singleInstanceUrl,
+	MIXER_URL: `${dashboardUrl}/#!/mixer`,
+	TEST_PANEL_URL: `${dashboardUrl}/panel/${bundleName}/test`,
+	PANEL_COMPONENTS_URL: `${dashboardUrl}/panel/${bundleName}/components`,
+	GRAPHIC_URL: `${dashboardUrl}/graphics/${bundleName}`,
+	SINGLE_INSTANCE_URL: `${dashboardUrl}/graphics/${bundleName}/single_instance.html`,
 	CONFIG: config
 };
