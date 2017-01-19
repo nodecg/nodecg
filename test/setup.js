f7889127fc5f8771c48f3586fa2abb59d6e46d00
Matthew McNamara <MattMcNam@users.noreply.github.com>
2017-01-19 19:58:42 +0100

    chore(tests): enable tests for pull requests

--- test/setup.js
+++ test/setup.js
@@ -34,46 +34,58 @@ before(function (done) {
 		e.apis.extension = e.server.getExtensions()[C.BUNDLE_NAME];
 
 		if (process.env.TRAVIS_OS_NAME && process.env.TRAVIS_JOB_NUMBER) {
-			console.log('Travis environment detected, running WebDriver.io with Travis capabilities');
-			const desiredCapabilities = {
-				name: `Travis job ${process.env.TRAVIS_JOB_NUMBER}`,
-				build: process.env.TRAVIS_BUILD_NUMBER,
-				tags: [process.env.TRAVIS_BRANCH, process.env.TRAVIS_COMMIT, process.env.TRAVIS_COMMIT_RANGE],
-				browserName: 'chrome',
-				chromeOptions: {
-					args: ['--disable-popup-blocking']
-				},
-				tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
-				loggingPrefs: {
-					browser: 'ALL'
+			const isPR = process.env.TRAVIS_PULL_REQUEST !== 'false';
+			if (isPR) {
+				console.log('Pull request detected, running WebDriver.io with local capabilities');
+				e.browser.client = webdriverio.remote({
+					desiredCapabilities: {
+						browserName: 'chrome',
+						chromeOptions: {
+							args: ['--no-sandbox']
+						},
+						loggingPrefs: {
+							browser: 'ALL'
+						}
+					}
+				});
+			} else {
+				console.log('Travis environment detected, running WebDriver.io with Travis capabilities');
+				const desiredCapabilities = {
+					name: `Travis job ${process.env.TRAVIS_JOB_NUMBER}`,
+					build: process.env.TRAVIS_BUILD_NUMBER,
+					tags: [process.env.TRAVIS_BRANCH, process.env.TRAVIS_COMMIT, process.env.TRAVIS_COMMIT_RANGE],
+					browserName: 'chrome',
+					chromeOptions: {
+						args: ['--disable-popup-blocking']
+					},
+					tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+					loggingPrefs: {
+						browser: 'ALL'
+					}
+				};
+
+				if (process.env.TRAVIS_TAG) {
+					desiredCapabilities.tags.push(process.env.TRAVIS_TAG);
 				}
-			};
 
-			if (process.env.TRAVIS_PULL_REQUEST !== 'false') {
-				desiredCapabilities.tags.push(process.env.TRAVIS_PULL_REQUEST);
+				if (process.env.TRAVIS_OS_NAME === 'linux') {
+					// desiredCapabilities.platform = 'Linux';
+
+					// Use Windows until Sauce Labs has at least Chrome 49 on Linux
+					// https://saucelabs.com/platforms/#linux
+					desiredCapabilities.platform = 'Windows 10';
+				} else if (process.env.TRAVIS_OS_NAME === 'osx') {
+					desiredCapabilities.platform = 'OS X 10.10';
+				}
+
+				e.browser.client = webdriverio.remote({
+					desiredCapabilities,
+					host: 'ondemand.saucelabs.com',
+					port: 80,
+					user: process.env.SAUCE_USERNAME,
+					key: process.env.SAUCE_ACCESS_KEY
+				});
 			}
-
-			if (process.env.TRAVIS_TAG) {
-				desiredCapabilities.tags.push(process.env.TRAVIS_TAG);
-			}
-
-			if (process.env.TRAVIS_OS_NAME === 'linux') {
-				// desiredCapabilities.platform = 'Linux';
-
-				// Use Windows until Sauce Labs has at least Chrome 49 on Linux
-				// https://saucelabs.com/platforms/#linux
-				desiredCapabilities.platform = 'Windows 10';
-			} else if (process.env.TRAVIS_OS_NAME === 'osx') {
-				desiredCapabilities.platform = 'OS X 10.10';
-			}
-
-			e.browser.client = webdriverio.remote({
-				desiredCapabilities,
-				host: 'ondemand.saucelabs.com',
-				port: 80,
-				user: process.env.SAUCE_USERNAME,
-				key: process.env.SAUCE_ACCESS_KEY
-			});
 		} else {
 			console.log('Running WebDriver.io with local capabilities');
 			e.browser.client = webdriverio.remote({
