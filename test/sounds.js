d38cbbeefc05f5e8738d7d618695d07d8e4a9c3c
Alex Van Camp <vancamp.alex@gmail.com>
2017-03-03 13:31:30 -0600

    fix(sounds): persist the soundCues replicant to disk
    Somehow, this feature went months without anyone noticing that these weren't being persisted!

--- test/sounds.js
+++ test/sounds.js
@@ -8,6 +8,99 @@ const assert = require('chai').assert;
 const e = require('./setup/test-environment');
 const C = require('./setup/test-constants');
 
+describe('sounds - soundCues replicant', () => {
+	it('should generate the correct defaultValue', () => {
+		const rep = e.apis.extension.Replicant('soundCues', 'test-bundle');
+		assert.deepEqual(rep.value, [{
+			name: 'name-only',
+			assignable: true,
+			file: null,
+			volume: 30
+		}, {
+			name: 'default-volume',
+			defaultVolume: 80,
+			assignable: true,
+			file: null,
+			volume: 80
+		}, {
+			name: 'non-assignable',
+			assignable: false,
+			file: null,
+			volume: 30
+		}, {
+			name: 'default-file',
+			defaultFile: {
+				sum: '9f7f2776691fdcb242c1fc72e115f5c24c63273c',
+				base: 'default-file.ogg',
+				ext: '.ogg',
+				name: 'default-file',
+				url: '/sound/test-bundle/default-file/default.ogg',
+				default: true
+			},
+			assignable: true,
+			file: {
+				sum: '9f7f2776691fdcb242c1fc72e115f5c24c63273c',
+				base: 'default-file.ogg',
+				ext: '.ogg',
+				name: 'default-file',
+				url: '/sound/test-bundle/default-file/default.ogg',
+				default: true
+			},
+			volume: 30
+		}]);
+	});
+
+	it('should remove any persisted cues that are no longer in the bundle manifest', () => {
+		const rep = e.apis.extension.Replicant('soundCues', 'remove-persisted-cues');
+		assert.deepEqual(rep.value, [{
+			name: 'persisted-cue-1',
+			assignable: true,
+			file: null,
+			volume: 30
+		}]);
+	});
+
+	it('should add any cues in the bundle manifest that aren\'t in the persisted replicant.', () => {
+		const rep = e.apis.extension.Replicant('soundCues', 'add-manifest-cues');
+		assert.deepEqual(rep.value, [{
+			name: 'persisted-cue-1',
+			assignable: true,
+			file: null,
+			volume: 30
+		}, {
+			name: 'manifest-cue-1',
+			assignable: true,
+			file: null,
+			volume: 30
+		}]);
+	});
+
+	it('should update any cues in that are in both in the persisted replicant and the bundle manifest.', () => {
+		const rep = e.apis.extension.Replicant('soundCues', 'update-cues');
+		assert.deepEqual(rep.value, [{
+			name: 'updated-cue',
+			defaultFile: {
+				base: 'default-file.ogg',
+				default: true,
+				ext: '.ogg',
+				name: 'default-file',
+				sum: '9f7f2776691fdcb242c1fc72e115f5c24c63273c',
+				url: '/sound/update-cues/updated-cue/default.ogg'
+			},
+			assignable: false,
+			file: {
+				sum: '9f7f2776691fdcb242c1fc72e115f5c24c63273c',
+				base: 'default-file.ogg',
+				ext: '.ogg',
+				name: 'default-file',
+				url: '/sound/update-cues/updated-cue/default.ogg',
+				default: true
+			},
+			volume: 30
+		}]);
+	});
+});
+
 describe('sounds - mixer page', function () {
 	this.timeout(10000);
 
@@ -22,7 +115,7 @@ describe('sounds - mixer page', function () {
 				.switchTab(e.browser.tabs.dashboard)
 				.then(() => new Promise((resolve, reject) => {
 					fs.copy('test/fixtures/assets/test-bundle/sounds/success.ogg',
-						path.join(C.ASSETS_ROOT, '/test-bundle/sounds/success.ogg'), {replace: true}, err => {
+						path.join(C.ASSETS_ROOT, 'test-bundle/sounds/success.ogg'), {replace: true}, err => {
 							if (err) {
 								reject(err);
 								return;
@@ -31,7 +124,7 @@ describe('sounds - mixer page', function () {
 							resolve();
 						});
 				}))
-				.selectorExecuteAsync('#cues > ncg-sound-cue:nth-child(1) > #select', (elements, done) => {
+				.selectorExecuteAsync('ncg-sounds[bundle-name="test-bundle"] ncg-sound-cue:nth-child(1) > #select', (elements, done) => {
 					const interval = setInterval(() => {
 						const options = elements[0].options;
 						if (options.length === 2 && options[1].value === 'success.ogg') {
@@ -84,7 +177,7 @@ describe('sounds - client api', function () {
 	// TODO: Figure out why this test is so flaky, and re-enable it.
 	it.skip('#playSound should play the right sound after changing away from and then back to a sound', done => {
 		e.browser.client
-			// Play success.ogg
+		// Play success.ogg
 			.switchTab(e.browser.tabs.dashboard)
 			.execute(() => {
 				const ncgSoundCue = document.querySelector('#cues > ncg-sound-cue:nth-child(4)');
