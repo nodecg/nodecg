26f2e06e58dee4de33d4e8fb3a90ee43f5c60c65
Alex Van Camp <vancamp.alex@gmail.com>
2017-04-28 10:15:29 -0500

    fix tests

--- test/setup/test-constants.js
+++ test/setup/test-constants.js
@@ -4,14 +4,15 @@ const path = require('path');
 const config = require(path.resolve(__dirname, '../../lib/config')).config;
 const bundleName = 'test-bundle';
 const dashboardUrl = `http://${config.baseURL}`;
+const TEST_BUNDLE_ROOT = `${dashboardUrl}/bundles/${bundleName}`;
 
 module.exports = {
 	BUNDLE_NAME: bundleName,
 	DASHBOARD_URL: dashboardUrl,
-	TEST_PANEL_URL: `${dashboardUrl}/panel/${bundleName}/panel.html`,
-	PANEL_COMPONENTS_URL: `${dashboardUrl}/panel/${bundleName}/components`,
-	GRAPHIC_URL: `${dashboardUrl}/graphics/${bundleName}`,
-	SINGLE_INSTANCE_URL: `${dashboardUrl}/graphics/${bundleName}/single_instance.html`,
+	TEST_PANEL_URL: `${TEST_BUNDLE_ROOT}/dashboard/panel.html`,
+	BUNDLE_BOWER_COMPONENTS_URL: `${TEST_BUNDLE_ROOT}/bower_components`,
+	GRAPHIC_URL: `${TEST_BUNDLE_ROOT}/graphics`,
+	SINGLE_INSTANCE_URL: `${TEST_BUNDLE_ROOT}/single_instance.html`,
 	CONFIG: config,
 	REPLICANTS_ROOT: path.join(process.env.NODECG_ROOT, 'db/replicants'),
 	ASSETS_ROOT: path.join(process.env.NODECG_ROOT, 'assets')
--- test/single_instance.js
+++ test/single_instance.js
@@ -24,7 +24,7 @@ describe('single-instance graphics', function () {
 			singleInstance.once('graphicAvailable', cb);
 
 			function cb(url) {
-				if (url === '/graphics/test-bundle/single_instance.html') {
+				if (url === '/bundles/test-bundle/graphics/single_instance.html') {
 					throw new Error('The graphic must have gotten redirected.');
 				}
 			}
