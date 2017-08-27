161c3a47a68c403155b3dd0811c75e6c68bc90aa
Alex Van Camp <vancamp.alex@gmail.com>
2017-08-26 21:28:30 -0500

    fix(dashboard): fix issues with Clipboard.js on Firefox
    Helps #328.

--- src/dashboard/elements/graphics/ncg-graphics.js
+++ src/dashboard/elements/graphics/ncg-graphics.js
@@ -65,21 +65,36 @@ class NcgGraphics extends Polymer.Element {
 	connectedCallback() {
 		super.connectedCallback();
 
-		Polymer.RenderStatus.afterNextRender(this, () => {
+		Polymer.RenderStatus.beforeNextRender(this, () => {
+			// 2017-08-26 Firefox seems to return an Array here instead of a NodeList.
+			// This is a problem, because Clipboard.js specifically doesn't accept Arrays (???).
+			// If `buttons` is an array, then we just iterate over it and make a separate Clipboard
+			// instance for each, rather than one instance for the whole collection.
 			const buttons = this.shadowRoot.querySelectorAll('.copyButton');
-			const clipboard = new Clipboard(buttons);
-			clipboard.on('success', () => {
-				this.$.copyToast.show('Graphic URL copied to clipboard.');
-			});
-			clipboard.on('error', e => {
-				this.$.copyToast.show('Failed to copy graphic URL to clipboard!');
-				console.error(e);
-			});
+			if (Array.isArray(buttons)) {
+				buttons.forEach(button => {
+					const clipboard = new Clipboard(button);
+					this._initClipboard(clipboard);
+				});
+			} else {
+				const clipboard = new Clipboard(buttons);
+				this._initClipboard(clipboard);
+			}
 		});
 	}
 
 	calcShortUrl(graphicUrl) {
 		return graphicUrl.split('/').slice(4).join('/');
+	}
+
+	_initClipboard(clipboard) {
+		clipboard.on('success', () => {
+			this.$.copyToast.show('Graphic URL copied to clipboard.');
+		});
+		clipboard.on('error', e => {
+			this.$.copyToast.show('Failed to copy graphic URL to clipboard!');
+			console.error(e);
+		});
 	}
 
 	_computeFullGraphicUrl(url) {
