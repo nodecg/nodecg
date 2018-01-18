acb168c971631b21d87f2fe5dd12dd326c260b6b
Alex Van Camp <vancamp.alex@gmail.com>
2018-01-17 21:57:46 -0600

    feat: add support for zeit pkg builds (#362)

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 process.title = 'NodeCG';
 global.exitOnUncaught = true;
 
-if (process.cwd() !== __dirname) {
-	console.warn('[nodecg] process.cwd is %s, expected %s', process.cwd(), __dirname);
+const cwd = process.cwd();
+global.isZeitPkg = __dirname.startsWith('/snapshot/') || __dirname.toLowerCase().startsWith('c:\\snapshot\\');
+if (global.isZeitPkg) {
+	console.info('[nodecg] Detected that NodeCG is running inside a ZEIT pkg (https://github.com/zeit/pkg)');
+} else if (cwd !== __dirname) {
+	console.warn('[nodecg] process.cwd is %s, expected %s', cwd, __dirname);
 	process.chdir(__dirname);
 	console.info('[nodecg] Changed process.cwd to %s', __dirname);
 }
