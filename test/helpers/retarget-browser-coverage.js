119030b6f174f4bf0921afa97ef14033fbb1be8d
Alex Van Camp <vancamp.alex@gmail.com>
2018-07-08 21:35:41 -0500

    test: ensure coverage from all iframes is extracted

---
+++ test/helpers/retarget-browser-coverage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const replace = require('replace-in-file');
+
+replace.sync({
+	files: 'instrumented/**/*.js',
+	from: /new Function\('return this'\)/g,
+	to: `new Function('return window.top')`
+});
