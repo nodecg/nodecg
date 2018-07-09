dfce675341b61faca92c14d1473c313fc087eb5a
Alex Van Camp <vancamp.alex@gmail.com>
2018-07-09 18:05:57 -0500

    test: improve login tests

--- test/login.js
+++ test/login.js
@@ -11,6 +11,10 @@ require('./helpers/nodecg-and-webdriver')(test, { // Must be first.
 const C = require('./helpers/test-constants');
 const e = require('./helpers/test-environment');
 
+test.beforeEach(async () => {
+	await e.browser.client.switchTab(e.browser.tabs.login);
+});
+
 test.serial('redirects unauthorized users to /login', async t => {
 	await e.browser.client.newWindow(C.DASHBOARD_URL);
 	const url = await e.browser.client.getUrl();
@@ -18,8 +22,6 @@ test.serial('redirects unauthorized users to /login', async t => {
 });
 
 test.serial('should deny access to bad credentials', async t => {
-	await e.browser.client.switchTab(e.browser.tabs.login);
-
 	await e.browser.client.waitForVisible('#username');
 	await e.browser.client.waitForVisible('#localForm');
 	await e.browser.client.waitForVisible('#password');
@@ -33,23 +35,21 @@ test.serial('should deny access to bad credentials', async t => {
 });
 
 test.serial('logging in should work', async t => {
-	await e.browser.client.switchTab(e.browser.tabs.login);
-
-	await e.browser.client.waitForVisible('#username');
-	await e.browser.client.waitForVisible('#localForm');
-	await e.browser.client.waitForVisible('#password');
-
-	await e.browser.client.setValue('#username', 'admin');
-	await e.browser.client.setValue('#password', 'password');
-	await e.browser.client.click('#localSubmit');
-
+	await logIn(t);
 	const url = await e.browser.client.getUrl();
 	t.is(url, C.DASHBOARD_URL);
 });
 
-test.serial('regenerating a token should send the user back to /login', async t => {
-	await e.browser.client.switchTab(e.browser.tabs.login);
+test.serial('logging out should work', async t => {
+	t.is(await e.browser.client.getUrl(), C.DASHBOARD_URL);
+	await logOut();
+	await e.browser.client.refresh();
+	t.is(await e.browser.client.getUrl(), C.LOGIN_URL);
+});
 
+test.serial('regenerating a token should send the user back to /login', async t => {
+	await logIn(t);
+	await e.browser.client.newWindow(C.DASHBOARD_URL);
 	t.is(await e.browser.client.getUrl(), C.DASHBOARD_URL);
 
 	// We need to preserve the coverage from this test, because it will be lost
@@ -57,9 +57,7 @@ test.serial('regenerating a token should send the user back to /login', async t 
 	const {value: coverage} = await e.browser.client.execute(() => {
 		const ncgSettings = document.querySelector('ncg-dashboard').shadowRoot
 			.querySelector('ncg-settings');
-		setTimeout(() => {
-			ncgSettings.resetKey();
-		});
+		ncgSettings.resetKey();
 		return window.__coverage__;
 	});
 
@@ -76,8 +74,42 @@ test.serial('regenerating a token should send the user back to /login', async t 
 	t.pass();
 });
 
-test.serial('logging out should work', async t => {
-	await e.browser.client.newWindow(`${C.ROOT_URL}logout`);
+test.serial('token invalidation should show an UnauthorizedError on open pages', async t => {
+	await e.browser.client.newWindow(C.DASHBOARD_URL);
+	await logIn(t);
+	t.is(await e.browser.client.getUrl(), C.DASHBOARD_URL);
+
+	await e.browser.client.newWindow(C.GRAPHIC_URL);
+	t.is(await e.browser.client.getUrl(), C.GRAPHIC_URL);
+	await e.browser.client.execute(() => {
+		window.socket.emit('regenerateToken', window.token);
+	});
+
+	await new Promise(resolve => {
+		setTimeout(() => {
+			resolve();
+		}, 1000);
+	});
+
 	const url = await e.browser.client.getUrl();
-	t.is(url, C.LOGIN_URL);
+	t.true(url.startsWith(`${C.ROOT_URL}authError?code=token_invalidated`));
 });
+
+async function logIn(t) {
+	t.is(await e.browser.client.getUrl(), C.LOGIN_URL);
+
+	await e.browser.client.waitForVisible('#username');
+	await e.browser.client.waitForVisible('#localForm');
+	await e.browser.client.waitForVisible('#password');
+
+	await e.browser.client.setValue('#username', 'admin');
+	await e.browser.client.setValue('#password', 'password');
+	await e.browser.client.click('#localSubmit');
+}
+
+async function logOut() {
+	const currentTabId = await e.browser.client.getCurrentTabId();
+	await e.browser.client.newWindow(`${C.ROOT_URL}logout`);
+	await e.browser.client.close();
+	await e.browser.client.switchTab(currentTabId);
+}
