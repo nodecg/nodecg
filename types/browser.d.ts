590688b4593139246907b6453a8c91fb617ae1b8
Hoishin <hoishinxii@gmail.com>
2018-08-18 21:07:32 +0900

    export global variables definition

--- types/browser.d.ts
+++ types/browser.d.ts
@@ -4,6 +4,7 @@ import {NodeCGBrowser} from './lib/nodecg-instance';
 declare const nodecg: NodeCGBrowser;
 declare const NodeCG: NodeCGStaticBrowser;
 
+export {nodecg, NodeCG}
 export * from './lib/config';
 export * from './lib/logger';
 export * from './lib/replicant';
