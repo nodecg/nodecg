a11a171629e7537c0fbc75b96885b2530f27c745
Dan Shields <d.shieldsuk@gmail.com>
2019-02-07 00:55:26 +0000

    Export more types in browser.d.ts

--- types/browser.d.ts
+++ types/browser.d.ts
@@ -4,8 +4,10 @@ import {NodeCGBrowser} from './lib/nodecg-instance';
 declare global {
 	export const nodecg: NodeCGBrowser;
 	export const NodeCG: NodeCGStaticBrowser;
+	export interface Window { nodecg: NodeCGBrowser, NodeCG: NodeCGStaticBrowser }
 }
 
+export {NodeCGStaticBrowser, NodeCGBrowser};
 export * from './lib/config';
 export * from './lib/logger';
 export * from './lib/replicant';
