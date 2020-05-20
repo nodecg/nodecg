fe22a7ddd5d78641d595922d1d2826d8c20d37a6
Alex Van Camp <vancamp.alex@gmail.com>
2020-05-20 15:48:31 -0500

    some degree of code coverage works

--- nyc.config.js
+++ nyc.config.js
@@ -1,5 +1,23 @@
 module.exports = {
+	/**
+	 * Start with the official TS config as a base.
+	 */
 	extends: '@istanbuljs/nyc-config-typescript',
+
+	/**
+	 * Ensure all files are considered for coverage, even if the file isn't used.
+	 * This helps catch unused/untested files.
+	 */
 	all: true,
+
+	/**
+	 * Ensure that the contents of the build dir are never considered for instrumentation and coverage.
+	 */
 	exclude: ['build/**'],
+
+	/**
+	 * When using nyc from the command line, only instrument the server files.
+	 * The client files are instrumented separately during their webpack build.
+	 */
+	include: ['src/server/**'],
 };
