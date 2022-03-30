5ccc9e0c4454b77df6a2133f7ddeca119cc4ad85
Keiichiro Amemiya <hoishinxii@gmail.com>
2022-03-30 21:21:41 +0900

    fix(types): correct more types

--- types/lib/replicant.d.ts
+++ types/lib/replicant.d.ts
@@ -29,7 +29,7 @@ export class ReplicantServer<V> extends ReplicantCommon<V> {
 }
 
 export class ReplicantBrowser<V> extends ReplicantCommon<V> {
-	static declareReplicants(): DeclaredReplicants<'browser'>;
+	static declaredReplicants(): DeclaredReplicants
 	constructor(
 		name: string,
 		namespace: string,
@@ -70,8 +70,8 @@ export interface OperationQueueItem {
 /**
  * Declared replicant store object
  */
-export interface DeclaredReplicants<P extends Platform> {
-	[bundleName: string]: {[replicantName: string]: Replicant<unknown, P>};
+export interface DeclaredReplicants {
+	[bundleName: string]: {[replicantName: string]: ReplicantBrowser<unknown>};
 }
 
 /**
