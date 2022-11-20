3c58533ada122c6b85cfd0d10146bdaf27278f59
Alex Van Camp <vancamp.alex@gmail.com>
2022-11-19 20:20:06 -0600

    test: disable CI-breaking AVA lint rule

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
 		project: './test/tsconfig.json',
 	},
 	rules: {
-		'ava/no-import-test-files': ['error', { files: ['test/**/*.js'] }],
+		'ava/no-import-test-files': 0, // can time out on Windows CI due to Windows being awful at handling many tiny files
 		'ava/use-test': 0, // we have legitimate use cases for not doing this
 		'@typescript-eslint/no-implicit-any-catch': 0, // too much of a hassle
 		'@typescript-eslint/ban-ts-comment': 0, // too much of a hassle
