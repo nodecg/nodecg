d7587413a48f6c70f1c87a9909fe367cc4c1b160
Alex Van Camp <vancamp.alex@gmail.com>
2023-01-16 09:21:00 -0600

    fix: add addListener and removeListener to typedemitter

--- src/shared/typed-emitter.ts
+++ src/shared/typed-emitter.ts
@@ -11,12 +11,21 @@ export type EventReceiver<T> = (params: T) => void;
 
 export class TypedEmitter<T extends EventMap> {
 	readonly _emitter = new EventEmitter();
+
+	addListener<K extends EventKey<T & Builtins>>(eventName: K, fn: (T & Builtins)[K]): void {
+		this._emitter.addListener(eventName, fn);
+	}
+
 	on<K extends EventKey<T & Builtins>>(eventName: K, fn: (T & Builtins)[K]): void {
 		this._emitter.on(eventName, fn);
 	}
 
 	off<K extends EventKey<T & Builtins>>(eventName: K, fn: (T & Builtins)[K]): void {
 		this._emitter.off(eventName, fn);
+	}
+
+	removeListener<K extends EventKey<T & Builtins>>(eventName: K, fn: (T & Builtins)[K]): void {
+		this._emitter.removeListener(eventName, fn);
 	}
 
 	emit<K extends EventKey<T & Builtins>>(eventName: K, ...params: Parameters<(T & Builtins)[K]>): void {
@@ -38,4 +47,6 @@ export class TypedEmitter<T extends EventMap> {
 	listeners<K extends EventKey<T & Builtins>>(eventName: K): Array<(T & Builtins)[K]> {
 		return this._emitter.listeners(eventName) as any;
 	}
+
+	// We intentionally don't expose removeAllListeners because it would break Replicants when used.
 }
