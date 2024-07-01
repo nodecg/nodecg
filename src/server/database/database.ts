b0b48eec705888db24758f3607186fcbfe8bf429
Grant J. Butler <grantjbutler@users.noreply.github.com>
2024-07-01 12:09:09 -0400

    Hook NodeCG's logging system into Drizzle.

--- src/server/database/database.ts
+++ src/server/database/database.ts
@@ -7,11 +7,18 @@ import Database from 'better-sqlite3';
 import { nodecgRootPath } from '../../shared/utils/rootPath';
 import createLogger from '../logger';
 import * as schema from './entity';
+import { DefaultLogger, LogWriter } from 'drizzle-orm';
 
 export const SUPERUSER_ROLE_ID = '07e18d80-fa74-4d98-ac18-838c745a480f';
 const PERMISSION_ID = '923561ef-4186-4370-b7df-f12e64fc7bd2';
 
 const log = createLogger('database');
+
+class NodeCGDrizzleLogger implements LogWriter {
+	write(message: string): void {
+		log.trace(message);
+	}
+}
 
 const dbPath = path.join(nodecgRootPath, 'db/nodecg.sqlite3');
 const migrationsPath = path.join(nodecgRootPath, 'db/migrations');
@@ -21,7 +28,10 @@ const testing = process.env.NODECG_TEST?.toLowerCase() === 'true';
 // When testing, we specifically use SQLite's in-memory storage.
 // This allows us to use SQLite as normal when running tests without any data being persisted on disk.
 const sqlite = new Database(testing ? ':memory:' : dbPath);
-const db = drizzle(sqlite, { schema: { ...schema.tables, ...schema.relations } });
+const db = drizzle(sqlite, {
+	logger: new DefaultLogger({ writer: new NodeCGDrizzleLogger() }),
+	schema: { ...schema.tables, ...schema.relations }
+});
 
 export async function initialize() {
 	if (testing) {
