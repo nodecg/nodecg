883fa89dca3889abba2c7838619f0c51e4beb201
Keiichiro Amemiya <hoishinxii@gmail.com>
2025-01-05 01:33:14 +0100

    uh

--- scripts/generate-types-package.ts
+++ scripts/generate-types-package.ts
@@ -1,39 +1,34 @@
+import { spawnSync } from "node:child_process";
 import * as fs from "node:fs";
 import * as path from "node:path";
 
 import appRootPath from "app-root-path";
-import spawn from "nano-spawn";
 
 const rootNodeModulesPath = appRootPath.resolve("node_modules");
 const tmpNodeModulesPath = appRootPath.resolve("tmp_node_modules");
 const outputDir = appRootPath.resolve("generated-types");
 
-void (async () => {
-	try {
-		fs.rmSync(path.join(outputDir, "node_modules"), {
-			recursive: true,
-			force: true,
-		});
+fs.rmSync(path.join(outputDir, "node_modules"), {
+	recursive: true,
+	force: true,
+});
 
-		// Install dependencies in the types package
-		await spawn("npm", ["i"], {
-			cwd: outputDir,
-			stdio: "inherit",
-		});
+// Install dependencies in the types package
+spawnSync("npm", ["i"], {
+	cwd: outputDir,
+	stdio: "inherit",
+	shell: true,
+});
 
-		// Without removing the root node_modules folder, tsc will use it along with the types package's node_modules
-		fs.cpSync(rootNodeModulesPath, tmpNodeModulesPath, { recursive: true });
-		fs.rmSync(rootNodeModulesPath, { recursive: true });
+// Without removing the root node_modules folder, tsc will use it along with the types package's node_modules
+fs.renameSync(rootNodeModulesPath, tmpNodeModulesPath);
 
-		// Test that the typings and dependencies are valid
-		await spawn("npx", ["tsc"], { cwd: outputDir, stdio: "inherit" });
+// Test that the typings and dependencies are valid
+spawnSync("npx", ["tsc"], {
+	cwd: outputDir,
+	stdio: "inherit",
+	shell: true,
+});
 
-		// Roll back the node_modules folder
-		fs.cpSync(tmpNodeModulesPath, rootNodeModulesPath, { recursive: true });
-	} finally {
-		// Clean up tmp_node_modules folder if generate() ended up with an error
-		if (fs.existsSync(tmpNodeModulesPath)) {
-			fs.rmSync(tmpNodeModulesPath, { recursive: true, force: true });
-		}
-	}
-})();
+// Roll back the node_modules folder
+fs.renameSync(tmpNodeModulesPath, rootNodeModulesPath);
