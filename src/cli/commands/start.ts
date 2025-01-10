611ae36f1b999c5f948e897f65e552d7d2564564
Keiichiro Amemiya <hoishinxii@gmail.com>
2025-01-10 18:44:46 +0100

    fix cwd handlign

--- src/cli/commands/start.ts
+++ src/cli/commands/start.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import path from "node:path";
 
 import { Command } from "commander";
@@ -10,18 +11,33 @@ export function startCommand(program: Command) {
 		.option("-d, --disable-source-maps", "Disable source map support")
 		.description("Start NodeCG")
 		.action(async () => {
-			const cwd = process.cwd();
+			const projectDir = recursivelyFindProject(process.cwd());
 
 			// Check if nodecg is already installed
-			if (pathContainsNodeCG(cwd)) {
-				await import(path.join(cwd, "index.js"));
+			if (pathContainsNodeCG(projectDir)) {
+				await import(path.join(projectDir, "index.js"));
 				return;
 			}
 
 			// Check if NodeCG is installed as a dependency
-			const nodecgDependencyPath = path.join(cwd, "node_modules/nodecg");
+			const nodecgDependencyPath = path.join(projectDir, "node_modules/nodecg");
 			if (pathContainsNodeCG(nodecgDependencyPath)) {
 				await import(path.join(nodecgDependencyPath, "index.js"));
 			}
 		});
+}
+
+function recursivelyFindProject(startDir: string) {
+	if (!path.isAbsolute(startDir)) {
+		throw new Error("startDir must be an absolute path");
+	}
+	const packageJsonDir = path.join(startDir, "package.json");
+	if (fs.existsSync(packageJsonDir)) {
+		return startDir;
+	}
+	const parentDir = path.dirname(startDir);
+	if (parentDir === startDir) {
+		throw new Error("Could not find a project directory");
+	}
+	return recursivelyFindProject(parentDir);
 }
