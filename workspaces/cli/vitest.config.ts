ef72e2402d920e1eecb6051ae6ed2c3de5867a46
Keiichiro Amemiya <hoishinxii@gmail.com>
2025-01-25 22:37:32 +0100

    ci: separate cli CI

--- workspaces/cli/vitest.config.ts
+++ workspaces/cli/vitest.config.ts
@@ -3,17 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		testTimeout: 15_000,
-		hookTimeout: 30_000,
-		maxWorkers: 1,
 		minWorkers: 1,
+		maxWorkers: 1,
 		env: {
 			test: "true",
 			NODECG_TEST: "true",
-		},
-		include: ["src/**/*.test.ts", "test/**/*.test.ts"],
-		coverage: {
-			include: ["src", "test"],
-			exclude: ["src/client"],
 		},
 	},
 });
