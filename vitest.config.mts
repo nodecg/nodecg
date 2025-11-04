639e5639d0e5463aaaf78fae94b7f35e1ee94ba8
Keiichiro Amemiya <kei@hoishin.dev>
2025-11-04 06:08:46 +0100

    test: parallel for unit tests (#884)

--- vitest.config.mts
+++ vitest.config.mts
@@ -2,10 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		testTimeout: 15_000,
-		hookTimeout: 30_000,
-		maxWorkers: 1,
-		minWorkers: 1,
 		env: {
 			test: "true",
 			NODECG_TEST: "true",
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,20 @@
-import { defineWorkspace } from "vitest/config";
+import { defineConfig, defineWorkspace } from "vitest/config";
 
-export default defineWorkspace([".", "./workspaces/*"]);
+export default defineWorkspace([
+	defineConfig({
+		test: {
+			name: "unit",
+			include: ["src/**/*.test.{ts,tsx}"],
+		},
+	}),
+	defineConfig({
+		test: {
+			name: "e2e",
+			include: ["test/**/*.test.{ts,tsx}"],
+			fileParallelism: false,
+			testTimeout: 15_000,
+			hookTimeout: 30_000,
+		},
+	}),
+	"./workspaces/*",
+]);
