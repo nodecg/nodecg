ddff0498aa9b7fe83d343ba8ca195e391372be22
Ewan Lyon <ewan@ewanlyon.com>
2025-11-21 23:54:08 +1100

    chore: remove commented out styles

--- workspaces/nodecg/src/client/dashboard/elements/sound/sound.tsx
+++ workspaces/nodecg/src/client/dashboard/elements/sound/sound.tsx
@@ -2,8 +2,6 @@ import { Accordion, Paper } from "@mantine/core";
 import { MasterFader } from "./master-fader";
 import { BundleSounds } from "./bundle-sounds";
 
-import styles from "./sound.module.css";
-
 export function Sound() {
 	const bundlesWithSounds = window.__renderData__.bundles.filter(
 		(bundle) => bundle.soundCues && bundle.soundCues.length > 0,
@@ -21,7 +19,7 @@ export function Sound() {
 			}}
 		>
 			<h1>Sound Mixer</h1>
-			<Paper p="md" className={styles["master-fader"]} miw={700}>
+			<Paper p="md" miw={700}>
 				<h2 style={{ marginTop: 0 }}>Master Fader</h2>
 				<MasterFader />
 			</Paper>
